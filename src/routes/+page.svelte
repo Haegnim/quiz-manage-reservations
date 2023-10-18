b5677866be597cbd3d532134eac861143caf34e6
Haegnim <haegnim@gmail.com>
2023-10-19 00:24:31 +0900

    Feat : store

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,47 @@
 <script lang="ts">
-  import Header from '../components/layout/header.svelte';
-  import Link from '../components/common/link.svelte';
-  import PlusIcon from '../../static/add.svg?raw'
-  import ReservationCard from '../components/reservationCard.svelte';
-  let title = 'Reservation'
-  let route = '/edit'
-  let customClass = 'orange'
-  let data = {
-    name : 'David',
-    phoneNumber: '021-523-4521',
-    guests: 3,
-    selectTable: [7,8],
-    memo: 'Birthday',
-    date: '2015-02-29',
-    params: '1'
-  }
+	import Header from '../components/layout/header.svelte';
+	import Link from '../components/common/link.svelte';
+	import PlusIcon from '../../static/add.svg?raw';
+	import ReservationCard from '../components/reservationCard.svelte';
+	import { reservation } from '../store/stores.js';
+	let title = 'Reservation';
+	let route = '/edit';
+	let customClass = 'orange';
+	let data = {
+		name: 'David',
+		phoneNumber: '021-523-4521',
+		guests: 3,
+		selectTable: [7, 8],
+		memo: 'Birthday',
+		date: '2015-02-29',
+		params: '1'
+	};
+	console.log(reservation);
 </script>
-<Header {title}>  
-  <Link {route} {customClass}>
-    {@html PlusIcon}
-    new reservation
-  </Link>
+
+<Header {title}>
+	<Link {route} {customClass}>
+		{@html PlusIcon}
+		new reservation
+	</Link>
 </Header>
 <div class="reservation-container">
-  <ul>
-
-    <ReservationCard 
-    {data}
-    />
-  </ul>
-
+	<ul>
+		{#each $reservation as reservation, index (index)}
+			<ReservationCard data={reservation} />
+		{/each}
+	</ul>
 </div>
+
 <style>
-.reservation-container{
-  background-color: #F5F5F4;
-  padding: 20px;
-  height: calc(100% - 90px);
-}
+	.reservation-container {
+		background-color: #f5f5f4;
+		padding: 20px;
+		height: calc(100% - 90px);
+	}
+	ul {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 20px;
+	}
 </style>
